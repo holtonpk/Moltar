b45ba386c5849bbf38dcbe4dcd16f37a01121651
Patrick Holton <patrickholton@Patricks-MBP.lan>
2024-03-09 00:45:57 -0500

    stuff 2

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,9 +1,4 @@
 import HeroText from "@/src/app/(marketing)/hero-text";
-import Analyze from "@/src/app/(marketing)/analyze";
-import Pricing from "@/src/app/(marketing)/components/plans";
-import FAQ from "@/src/app/(marketing)/pricing/faq";
-import Features from "@/src/app/(marketing)/features";
-import Stats from "@/src/app/(marketing)/stats";
 
 export default async function IndexPage() {
   return (
