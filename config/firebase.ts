8008ca773e14bd24059c7a0f4e8c03797a33e06a
Patrick Holton <patrickholton@Patricks-MBP.lan>
2024-03-12 12:29:00 -0400

    env

--- config/firebase.ts
+++ config/firebase.ts
@@ -4,13 +4,13 @@ import {getAuth} from "firebase/auth";
 import {getAnalytics} from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAmTbMLQFEcE8_pJdX4IU4pR2YpovfFFw0",
-  authDomain: "moltar-bc665.firebaseapp.com",
-  projectId: "moltar-bc665",
-  storageBucket: "moltar-bc665.appspot.com",
-  messagingSenderId: "1004338882157",
-  appId: "1:1004338882157:web:87c891ff8ce5623af58164",
-  measurementId: "G-JX19971GHG",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
