26972f132e6e1ec69925df81752153e5e4dcaeea
Patrick Holton <patrickholton@Patricks-MBP.lan>
2024-03-24 16:34:22 -0400

    responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="max-h-screen overflow-hidden">{children}</body>
     </html>
   );
 }
