181c339463cb11726f574b54c76954387b77fb19
Patrick Holton <patrickholton@Patricks-MBP.lan>
2024-03-24 22:21:52 -0400

    fav

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,4 +1,11 @@
 import HeroText from "@/src/app/(marketing)/hero-text";
+import {constructMetadata} from "@/lib/utils";
+
+export const metadata = constructMetadata({
+  title: "Moltar",
+  description: "Upload and ask away, Moltar will do the rest!",
+  image: "image/favicon.ico",
+});
 
 export default async function IndexPage() {
   return (
