234886854cd17707b835c1e702db2670f71ae5cd
Patrick Holton <patrickholton@Patricks-MBP.lan>
2024-03-27 13:22:32 -0400

    env update

--- src/app/api/convert-pdf-to-image/route.ts
+++ src/app/api/convert-pdf-to-image/route.ts
@@ -11,7 +11,9 @@ export async function POST(req) {
     // Creates a client
     const client = new vision.ImageAnnotatorClient({
       projectId: "moltar-bc665",
-      keyFilename: "public/moltar-bc665-483e23d48c4b.json",
+      credentials: JSON.parse(
+        process.env.NEXT_PUBLIC_FIREBASE_SERVICE_ACCOUNT_KEY
+      ),
     });
 
     /**
@@ -65,64 +67,66 @@ export async function POST(req) {
   }
 }
 
-export async function GET() {
-  try {
-    // Imports the Google Cloud client libraries
-    const vision = require("@google-cloud/vision").v1;
+// export async function GET() {
+//   try {
+//     // Imports the Google Cloud client libraries
+//     const vision = require("@google-cloud/vision").v1;
 
-    // Creates a client
-    const client = new vision.ImageAnnotatorClient({
-      projectId: "moltar-bc665",
-      keyFilename: "public/moltar-bc665-483e23d48c4b.json",
-    });
+//     // Creates a client
+//     const client = new vision.ImageAnnotatorClient({
+//       projectId: "moltar-bc665",
+//       credentials: JSON.parse(
+//         process.env.NEXT_PUBLIC_FIREBASE_SERVICE_ACCOUNT_KEY
+//       ),
+//     });
 
-    /**
-     * TODO(developer): Uncomment the following lines before running the sample.
-     */
-    // Bucket where the file resides
-    const bucketName = "moltar-bc665.appspot.com";
-    // Path to PDF file within bucket
-    // The folder to store the results
-    const outputPrefix = "scanned-documents";
-    // const fileName = "4ym68it";
-    const fileName = "081wc6";
-    const gcsSourceUri = `gs://${bucketName}/${fileName}`;
-    const gcsDestinationUri = `gs://${bucketName}/${fileName}/`;
+//     /**
+//      * TODO(developer): Uncomment the following lines before running the sample.
+//      */
+//     // Bucket where the file resides
+//     const bucketName = "moltar-bc665.appspot.com";
+//     // Path to PDF file within bucket
+//     // The folder to store the results
+//     const outputPrefix = "scanned-documents";
+//     // const fileName = "4ym68it";
+//     const fileName = "081wc6";
+//     const gcsSourceUri = `gs://${bucketName}/${fileName}`;
+//     const gcsDestinationUri = `gs://${bucketName}/${fileName}/`;
 
-    const inputConfig = {
-      // Supported mime_types are: 'application/pdf' and 'image/tiff'
-      mimeType: "application/pdf",
-      gcsSource: {
-        uri: gcsSourceUri,
-      },
-    };
-    const outputConfig = {
-      gcsDestination: {
-        uri: gcsDestinationUri,
-      },
-    };
-    const features = [{type: "DOCUMENT_TEXT_DETECTION"}];
-    const request = {
-      requests: [
-        {
-          inputConfig: inputConfig,
-          features: features,
-          outputConfig: outputConfig,
-        },
-      ],
-    };
+//     const inputConfig = {
+//       // Supported mime_types are: 'application/pdf' and 'image/tiff'
+//       mimeType: "application/pdf",
+//       gcsSource: {
+//         uri: gcsSourceUri,
+//       },
+//     };
+//     const outputConfig = {
+//       gcsDestination: {
+//         uri: gcsDestinationUri,
+//       },
+//     };
+//     const features = [{type: "DOCUMENT_TEXT_DETECTION"}];
+//     const request = {
+//       requests: [
+//         {
+//           inputConfig: inputConfig,
+//           features: features,
+//           outputConfig: outputConfig,
+//         },
+//       ],
+//     };
 
-    const [operation] = await client.asyncBatchAnnotateFiles(request);
-    const [filesResponse] = await operation.promise();
-    const destinationUri =
-      filesResponse.responses[0].outputConfig.gcsDestination.uri;
-    console.log("Json saved to: " + destinationUri);
+//     const [operation] = await client.asyncBatchAnnotateFiles(request);
+//     const [filesResponse] = await operation.promise();
+//     const destinationUri =
+//       filesResponse.responses[0].outputConfig.gcsDestination.uri;
+//     console.log("Json saved to: " + destinationUri);
 
-    return NextResponse.json({
-      destinationUri,
-    });
-  } catch (error) {
-    console.error("Error:", error);
-    return NextResponse.json({message: error.message});
-  }
-}
+//     return NextResponse.json({
+//       destinationUri,
+//     });
+//   } catch (error) {
+//     console.error("Error:", error);
+//     return NextResponse.json({message: error.message});
+//   }
+// }
