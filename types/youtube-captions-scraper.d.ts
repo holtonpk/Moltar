3e04cea20f0870dfc5861699d2941e2cbf9e00ef
Patrick Holton <patrickholton@Patricks-MBP.lan>
2024-04-05 01:27:31 -0400

    youtube api type

---
+++ types/youtube-captions-scraper.d.ts
@@ -0,0 +1,5 @@
+// youtube-captions-scraper.d.ts
+declare module "youtube-captions-scraper" {
+  export function getSubtitles(options: any): Promise<any>;
+  // Add other functions or types as necessary
+}
