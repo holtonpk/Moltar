dc53c3002fdaa63d034efccf32fa1c6ed2374a04
Patrick Holton <patrickholton@Patricks-MBP.lan>
2024-04-19 17:19:05 -0400

    mobile sing in size

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,10 @@ export const metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1"
+      ></meta>
       <Analytics />
       <body className="bg-background">{children}</body>
     </html>
